c2a1b9e690ff85511f5dcdda2eed69194998df6d
bemadeveloper <akbermet1990@icloud.com>
2024-01-14 21:24:19 +0600

    Clean comments

--- MyProjectForHomework/ViewController.swift
+++ MyProjectForHomework/ViewController.swift
@@ -1,10 +1,3 @@
-//
-//  ViewController.swift
-//  MyProjectForHomework
-//
-//  Created by Bema on 14/1/24.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
@@ -12,7 +5,6 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         setupView()
-        // Do any additional setup after loading the view.
     }
     
     func setupView() {
